8b59e27018febb00d1575c181327ffe635503f55
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-04-22 19:52:47 -0300

    Add Explore and ExploreCard

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,17 +1,20 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
 
 interface RootProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const RootLayout = ({ children }: RootProps) => (
   <html lang="en">
     <head>
       <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      <link
+        rel="stylesheet"
+        href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+      />
     </head>
     <body>{children}</body>
   </html>
-);
+)
 
-export default RootLayout;
+export default RootLayout
--- client/components/CustomTexts.tsx
+++ client/components/CustomTexts.tsx
@@ -4,23 +4,32 @@ import { motion } from 'framer-motion'
 import { fadeIn, textContainer, textVariant2 } from '../utils/motion'
 
 export type CustomTextProps = {
-  title: string
+  title: string | JSX.Element
   textStyles: string
 }
 
-export const TypingText = ({ title, textStyles }: CustomTextProps) => (
-  <motion.p
-    variants={textContainer}
-    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
-  >
-    {Array.from(title).map((letter, index) => (
-      <motion.span variants={textVariant2} key={index}>
-        {letter === '' ? '\u00A0' : letter}
-      </motion.span>
-    ))}
-  </motion.p>
-)
-
+export const TypingText = ({ title, textStyles }: CustomTextProps) => {
+  const letters = typeof title === 'string' ? Array.from(title) : [title]
+  return (
+    <motion.p
+      variants={textContainer}
+      className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+    >
+      {letters.map((letter, index) => (
+        <motion.span variants={textVariant2} key={index}>
+          {typeof letter === 'string' && letter === '' ? '\u00A0' : letter}
+        </motion.span>
+      ))}
+    </motion.p>
+  )
+}
 export const TitleText = ({ title, textStyles }: CustomTextProps) => (
-  <h2>Title Text</h2>
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >
+    {title}
+  </motion.h2>
 )
