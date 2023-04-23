abe4079165809d81eef28a1488cf180907dfe061
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-04-23 03:42:39 -0300

    Add NewFeatures and WhatsNew

--- client/components/NewFeatures.tsx
+++ client/components/NewFeatures.tsx
@@ -1,3 +1,31 @@
-const NewFeatures = () => <div>New Features</div>
+'use client'
+
+import styles from '../styles'
+
+export type NewFeaturesProps = {
+  imgUrl: string
+  title: string
+  subtitle: string
+}
+
+const NewFeatures = ({ imgUrl, title, subtitle }: NewFeaturesProps) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div
+      className={`${styles.flexCenter} h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]`}
+    >
+      <img
+        src={imgUrl}
+        alt={`Icon - ${title}`}
+        className="w-1/2 h-1/2 object-contain"
+      />
+    </div>
+    <h1 className="mt-[26px] font-bold text-[24px] leading-[30px] text-white">
+      {title}
+    </h1>
+    <p className="flex-1 mt-[16px] font-normal text-[18px] text-[#B0B0B0] leading-[28px]">
+      {subtitle}
+    </p>
+  </div>
+)
 
 export default NewFeatures
