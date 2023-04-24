1b2a75ad75ab926c284206442b94cf4484af6bc6
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-04-24 07:36:42 -0300

    Add World structure

--- client/sections/World.tsx
+++ client/sections/World.tsx
@@ -1,3 +1,62 @@
-const World = () => <section>World section</section>
+'use client'
+
+import { motion } from 'framer-motion'
+import { staggerContainer, fadeIn } from '../utils/motion'
+import { NewFeatures, TitleText, TypingText } from '../components'
+
+import styles from '../styles'
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People on the World" textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        textStyles="text-center"
+      />
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="Map with bullet informations" />
+
+        <div className="absolute bottom-20 right-20 w-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-01.png"
+            alt="It shows a people with an orange background"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div className="absolute top-10 left-20 w-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-02.png"
+            alt="It shows a people with an pink background"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div className="absolute top-1/2 right-[45%] w-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img
+            src="/people-03.png"
+            alt="It shows a people with cap and blue background"
+            className="w-full h-full"
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+)
 
 export default World
